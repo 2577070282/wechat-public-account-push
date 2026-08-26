10a2be64b7e06930331fee073693574d5c55463f
南有先生 <48191573+2577070282@users.noreply.github.com>
2026-08-26 09:03:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,37 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDBRt69T7WvnuEvCHXVgRLGbDxMA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'tPy_xWWN1oGZr9N8an-y1C9dol6gg5S_RM1u4dphVzk',
+      useTemplateId: 'CMs1FyPnBxBwwuQGXsQcUi1Zqx6JlKORTM9_WNJKZP4',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '08-17',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '08-27',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1996', date: '08-18',
+        },
+        {
+          type: '节日', name: '结婚纪念日', year: '2025', date: '05-19',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-01-22' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2025-05-19' },
+      ],
+    },
+        {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oDBRt6-IefOU4Jb5wLMIN51g7Uxw',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'CMs1FyPnBxBwwuQGXsQcUi1Zqx6JlKORTM9_WNJKZP4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-17',
       festivals: [
