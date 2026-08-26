97d5e9ad2ba0a91287120f44e6255e9c0aebfa75
南有先生 <48191573+2577070282@users.noreply.github.com>
2026-08-26 09:17:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDBRt69T7WvnuEvCHXVgRLGbDxMA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Cqx3duZ-A6iqPON6ppZzFTDdk8hEz3yW1Irjfet9Zt0',
+      useTemplateId: 'ZRvUBqsqsP1NERI3ZgU5DSeinbpqVhgQbH364E40ZuI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-17',
       festivals: [
